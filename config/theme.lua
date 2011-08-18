5f78fd2d09d61b6d32882e09e1cfc568cb4f08fa
karottenreibe <k@rottenrei.be>
2011-08-18 16:45:09 +0200

    removed useless theme options

--- config/theme.lua
+++ config/theme.lua
@@ -32,20 +32,9 @@ theme.menu_title_bg             = "#fff"
 theme.menu_primary_title_fg     = "#f00"
 theme.menu_secondary_title_fg   = "#666"
 
--- Proxy manager
-theme.proxy_active_menu_fg      = '#000'
-theme.proxy_active_menu_bg      = '#FFF'
-theme.proxy_inactive_menu_fg    = '#888'
-theme.proxy_inactive_menu_bg    = '#FFF'
-
 -- Statusbar specific
 theme.sbar_fg         = "#fff"
 theme.sbar_bg         = "#000"
-
--- Downloadbar specific
-theme.dbar_fg         = "#fff"
-theme.dbar_bg         = "#000"
-theme.dbar_error_fg   = "#F00"
 
 -- Input bar specific
 theme.ibar_fg           = "#000"
@@ -60,10 +49,6 @@ theme.selected_bg       = "#000"
 theme.selected_ntheme   = "#ddd"
 theme.loading_fg        = "#33AADD"
 theme.loading_bg        = "#000"
-
--- Trusted/untrusted ssl colours
-theme.trust_fg          = "#0F0"
-theme.notrust_fg        = "#F00"
 
 return theme
 -- vim: et:sw=4:ts=8:sts=4:tw=80
