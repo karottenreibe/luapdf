b20a511fdfedc14c8f04c2eb43f2ed0c3c960023
karottenreibe <git@rottenrei.be>
2011-09-01 22:56:04 +0200

    put visible calculation into separate function

--- widgets/document/render.c
+++ widgets/document/render.c
@@ -19,6 +19,26 @@
  *
  */
 
+static gboolean
+viewport_coordinates_get_visible(cairo_rectangle_int_t *r, document_data_t *d)
+{
+    /* calculate visible region */
+    GtkWidget *w = d->widget;
+    gdouble width = w->allocation.width;
+    gdouble height = w->allocation.height;
+    cairo_rectangle_int_t rect = {
+        d->hadjust->value * d->zoom,
+        d->vadjust->value * d->zoom,
+        width,
+        height,
+    };
+    cairo_region_t *visible_r = cairo_region_create_rectangle(&rect);
+
+    gboolean visible = (cairo_region_contains_rectangle(visible_r, r) != CAIRO_REGION_OVERLAP_OUT);
+    cairo_region_destroy(visible_r);
+    return visible;
+}
+
 static void
 page_render(cairo_t *c, page_info_t *i)
 {
@@ -36,17 +56,6 @@ document_render(document_data_t *d)
 {
     GtkWidget *w = d->widget;
 
-    /* calculate visible region */
-    gdouble width = w->allocation.width;
-    gdouble height = w->allocation.height;
-    cairo_rectangle_int_t rect = {
-        d->hadjust->value * d->zoom,
-        d->vadjust->value * d->zoom,
-        width,
-        height,
-    };
-    cairo_region_t *visible_r = cairo_region_create_rectangle(&rect);
-
     /* render recorded data directly to widget */
     cairo_t *c = gdk_cairo_create(gtk_widget_get_window(w));
     cairo_set_source_rgb(c, 1.0/256*220, 1.0/256*218, 1.0/256*213);
@@ -61,14 +70,13 @@ document_render(document_data_t *d)
             p->rectangle->width * d->zoom,
             p->rectangle->height * d->zoom,
         };
-        if (cairo_region_contains_rectangle(visible_r, &page_rect) != CAIRO_REGION_OVERLAP_OUT) {
+        if (viewport_coordinates_get_visible(&page_rect, d)) {
             cairo_scale(c, d->zoom, d->zoom);
             cairo_translate(c, p->rectangle->x - d->hadjust->value, p->rectangle->y - d->vadjust->value);
             page_render(c, p);
             cairo_identity_matrix(c);
         }
     }
-    cairo_region_destroy(visible_r);
     cairo_destroy(c);
 }
 
